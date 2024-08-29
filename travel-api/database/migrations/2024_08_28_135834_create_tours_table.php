5e6e7a80d83caba25c0b4e0b2ab7aec1b7061e5b
= <augustodemeloh@protonmail.com>
2024-08-29 01:23:56 -0300

    feat: add create_table migrations

---
+++ travel-api/database/migrations/2024_08_28_135834_create_tours_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tours', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('travel_id');
+            $table->string('name');
+            $table->date('starting_date');
+            $table->date('ending_date');
+            $table->integer('price');
+            $table->timestamps();
+
+            $table->foreign('travel_id')->references('id')->on('travels');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tours');
+    }
+};
