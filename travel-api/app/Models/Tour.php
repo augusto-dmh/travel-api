4a51940c4cc3a16315ecd5fadbc599e9b30f4e07
= <augustodemeloh@protonmail.com>
2024-08-29 01:37:35 -0300

    feat: add models for each table

---
+++ travel-api/app/Models/Tour.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Tour extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'travel_id',
+        'name',
+        'starting_date',
+        'ending_date',
+        'price'
+    ];
+
+    public function travel(): BelongsTo {
+        return $this->belongsTo(Travel::class);
+    }
+}
