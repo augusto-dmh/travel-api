72f877ce252f17b27abb6349e6abe2397d00f6f3
= <augustodemeloh@protonmail.com>
2024-09-01 16:35:03 -0300

    refactor: modify endpoints-related code to api logic
    Instead of building up a project following default Laravel MVC pattern, using web routes and 'web controllers' that return views, now the application is only focused on returning json responses - it's mainly a rest api for now, according to the mentorship.

--- travel-api/routes/web.php
+++ travel-api/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\TravelController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,7 +16,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/travels', [TravelController::class, 'index'])->name('travel.index');
 
 require __DIR__.'/auth.php';
