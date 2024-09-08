5c833d6467d3f81715cfceafce2a6848bdd1c2c8
= <augustodemeloh@protonmail.com>
2024-09-07 23:39:35 -0300

    feat: add endpoint for creating a travel

--- travel-api/app/Http/Controllers/Api/V1/TravelController.php
+++ travel-api/app/Http/Controllers/Api/V1/TravelController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Models\Travel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTravelRequest;
 use App\Http\Resources\TravelResource;
 
 class TravelController extends Controller
@@ -30,9 +31,11 @@ class TravelController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreTravelRequest $request)
     {
-        //
+        $travel = Travel::create($request->validated());
+
+        return new TravelResource($travel);
     }
 
     /**
--- travel-api/routes/api_v1.php
+++ travel-api/routes/api_v1.php
@@ -6,5 +6,6 @@ use App\Http\Controllers\Api\V1\TravelController;
 use App\Http\Controllers\CreateUserController;
 
 Route::get('/travels', [TravelController::class, 'index'])->name('api_v1.travel.index');
+Route::post('/travels', [TravelController::class, 'store'])->middleware('auth:sanctum')->name('api_v1.travel.store');
 Route::get('/travels/{travel:slug}/tours', [TourController::class, 'index'])->name('api_v1.tour.index');
 Route::post('/create-user', CreateUserController::class)->middleware('auth:sanctum')->name('api_v1.create_user');
