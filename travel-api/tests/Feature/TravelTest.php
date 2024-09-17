d7e3a03ed0b59efbfea8dc17bdcc2eff28bc8e74
= <augustodemeloh@protonmail.com>
2024-09-17 01:08:14 -0300

    fix: correct test for checking if public paginated travels are returned due to wrong precision
    The expected response data contained all the public travels, but since our pagination only allows 10 by page, whenever the public travels amount is greater than 10, the test fails: not all public travels are returned in the response, just the first 10. Now we estipulate exactly 10 travels to be created in the test, 5 non-public and 5 public, and iterate over all the public ones to see if they're present in the response (if the application is ok they'll ever be present: 5 is always less than 10, so they're in the same page, thus inside the json response).

--- travel-api/tests/Feature/TravelTest.php
+++ travel-api/tests/Feature/TravelTest.php
@@ -29,16 +29,19 @@ class TravelTest extends TestCase
     }
 
     public function test_public_paginated_travels_are_returned(): void {
-        $travels = Travel::factory()->count(15)->create();
-        $publicTravels = $travels->filter(fn($travel) => $travel->is_public);
-        $expectedResponseData = TravelResource::collection($publicTravels)->resolve();
+        $privateTravels = Travel::factory()->count(5)->create(['is_public' => false]);
+        $publicTravels = Travel::factory()->count(5)->create(['is_public' => true]);
+        // $expectedResponseData = TravelResource::collection($publicTravels)->resolve();
 
         $response = $this->get(route('api_v1.travel.index'));
 
         $response->assertStatus(200);
         $response->assertJsonPath('meta.per_page', 10);
         $response->assertJsonPath('meta.total', count($publicTravels));
-        $response->assertJson(['data' => $expectedResponseData]);
+        foreach ($publicTravels as $publicTravel) {
+            $response->assertJsonFragment(['name' => $publicTravel->name]);
+        }
+        // $response->assertJson(['data' => $expectedResponseData]);
     }
 
     function test_it_can_be_created_by_admin(): void {
